e0180992966894fe7d7a8c1575b30e48a1808ea1
mertcakmak2 <mertcakmak2@gmail.com>
2022-09-29 16:55:42 +0300

    additional

---
+++ conditions/switch.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	exp := 61
+	switch exp {
+	case 26:
+		fmt.Println("26")
+	case 61:
+		fmt.Println("61")
+	default:
+		fmt.Println("default")
+	}
+
+}
