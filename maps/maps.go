7b782f95cf38209e234c62b3eed11b699fd85e37
mertcakmak2 <mertcakmak2@gmail.com>
2022-10-18 11:42:04 +0300

    arrays,maps, select_channel

--- maps/maps.go
+++ maps/maps.go
@@ -3,5 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("maps")
+
+	m := make(map[string]int)
+	//or
+	// m := map[string]int{}
+	m["k1"] = 7
+	m["k2"] = 13
+	fmt.Println("map:", m)
+
+	// val := map[string]interface{}{}
+	// or
+	val := make(map[string]interface{})
+	val["name"] = "mert"
+	val["lastname"] = "cakmak"
+	val["age"] = 24
+
+	fmt.Printf("val: %v\n", val)
+	fmt.Printf("val[\"name\"]: %v\n", val["name"])
+
+	// or
+	val2 := map[string]interface{}{
+		"name":     "mert",
+		"lastname": "cakmak",
+		"age":      24,
+	}
+	fmt.Printf("val2: %v\n", val2)
 }
