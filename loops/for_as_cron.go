9ef83c576e42bf69fb39ded1c037532035bce73f
mertcakmak2 <mertcakmak2@gmail.com>
2022-10-20 08:56:45 +0300

    for as cron

--- loops/for_as_cron.go
+++ loops/for_as_cron.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 )
 
 func main() {
+	fmt.Printf("os.Args[1]: %v\n", os.Args[1])
 	for range time.Tick(time.Second * 3) {
 		time := time.Now().Format("15:04:05")
 		fmt.Println("Time: " + time)
